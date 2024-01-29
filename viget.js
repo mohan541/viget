451f5e16f5f08d3bbdbec147b3009a65cf5ff308
mohan541 <annavarapumohan143@gmail.com>
2024-01-29 12:20:56 +0530

    commit

--- viget.js
+++ viget.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
   $(".close").hide();
   $(".bar-contact-details").hide();
+  $(".bar-details").hide();
+  $(".bar-contact-details-main").hide();
   $(".display-bar").click(function () {
     $(".nav-list").hide();
     $(".main").hide();
@@ -8,9 +10,12 @@ $(document).ready(function () {
     $(".viget-contact").hide();
     $(".viget-footer").hide();
     $(".signup-newsletter").hide();
-
+    $(".viget-signup").hide();
     $(".close").show();
+    $(".bar-contact-details-main").show();
     $(".bar-contact-details").show();
+    // -----------------------------
+    // $(".nav-bar").hide();
   });
   $(".close").click(function () {
     $(".nav-list").show();
